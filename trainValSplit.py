65929e1d4e1c5f86bd35cac2e7ddebdb85ca470f
tikiwings <donaldsanchez7870@gmail.com>
2019-12-13 15:31:28 -0800

    Train and validation folders created.

--- trainValSplit.py
+++ trainValSplit.py
@@ -2,10 +2,12 @@ import shutil
 import os
 
 BASEPATH    = os.getcwd() + "/ourSpec"
-TRAINPATH   = BASEPATH + "/train/"
-VALPATH     = BASEPATH + "/val/"
+#TRAINPATH   = BASEPATH + "/train/"
+#VALPATH     = BASEPATH + "/val/"
+TRAINPATH   = os.getcwd() + "/train/"
+VALPATH     = os.getcwd() + "/val/"
 
-splitRatio = 0.2
+splitRatio = 0.8 #ex: 0.8 = 80% -> train | 20% -> val
 
 #print(BASEPATH)
 
@@ -16,16 +18,18 @@ for birdClass in classList:
    birdPath = BASEPATH + "/" + birdClass
    birdFiles = os.listdir(birdPath)
    
-   splitIndex = len(birdFiles) // splitRatio
+   splitIndex = int(len(birdFiles) * splitRatio)
    trainFiles = birdFiles[:splitIndex]
    valFiles   = birdFiles[splitIndex:]
    
    #move train and val files
    for f in trainFiles:
-      shutil.move(birdPath + "/" + f, TRAINPATH)
+      #shutil.move(birdPath + "/" + f, TRAINPATH + birdClass)
+      shutil.copy(birdPath + "/" + f, TRAINPATH + birdClass)
 
    for f in valFiles:
-      shutil.move(birdPath + "/" + f, VALPATH)
+      #shutil.move(birdPath + "/" + f, VALPATH + birdClass)
+      shutil.copy(birdPath + "/" + f, VALPATH + birdClass)
 
 
 
